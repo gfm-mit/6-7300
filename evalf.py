93a13e3bc97d049363ba0bae3542452e241c0385
Jillian Ross <jillianr@mit.edu>
2023-09-25 09:09:36 -0400

    Dimensions work out, but need to test with actual data

--- evalf.py
+++ evalf.py
@@ -7,21 +7,21 @@ def generate_inputs(n, E):
     :return: state vector x, parameters p, inputs u
     """
     # Placeholders
-    y = np.zeros((n, 1))                    # n x 1
-    tilde_y = np.zeros((n, 1))              # n x 1
-    mu = np.zeros((n, 1))                   # n x 1
-    tau1 = np.zeros((n, 1))                 # n x 1
-    tau2 = np.zeros((n, 1))                 # n x 1
-    omega = np.zeros((n, 1))                # n x 1
-    alpha = np.zeros((n, 1))                # n x 1
-    gamma2 = np.zeros((E.shape[0], 1))      # nm x 1
-    d = np.zeros((E.shap[0], 1))            # nm x 1
-    g = np.zeros((n, 1))                    # n x 1 (little y)
-    delt_w = np.zeros((n, 1))               # n x 1
+    y = np.ones((n, 1))                    # n x 1
+    tilde_y = np.ones((n, 1))              # n x 1
+    mu = np.ones((n, 1))                   # n x 1
+    tau1 = np.ones((n, 1))                 # n x 1
+    tau2 = np.ones((n, 1))                 # n x 1
+    omega = np.ones((n, 1))                # n x 1
+    alpha = np.ones((n, 1))                # n x 1
+    gamma2 = np.ones((E.shape[0], 1))      # nm x 1
+    d = np.ones((E.shape[0], 1))           # nm x 1
+    g = np.ones((n, 1))                    # n x 1 (little y)
+    delt_w = np.ones((n, 1))               # n x 1
     # Build x, p, u arrays
-    x = np.array([y, tilde_y, mu])
-    p = np.array([tau1, tau2, omega, alpha, gamma2, d, g])
-    u = np.array([delt_w])
+    x = [y, tilde_y, mu]
+    p = [tau1, tau2, omega, alpha, gamma2, d, g]
+    u = [delt_w]
     return x, p, u
 
 
@@ -51,7 +51,7 @@ def evalf(x, p, u, E):
     """
     delt_true_currency = get_delt_true_currency(mu=x[2], omega=p[2], delt_W=u[0], Y=x[0])
     delt_eff_currency = get_eff_currency(Y=x[0], tilde_Y=x[1], tau1=p[0])
-    delt_currency_drift = get_currency_drift(tilde_Y=x[1], alpha=p[3], tau2=p[1], E=E)
+    delt_currency_drift = get_currency_drift(tilde_Y=x[1], alpha=p[3], tau2=p[1], g=p[6], E=E, d=p[5])
     return np.array([delt_true_currency, delt_eff_currency, delt_currency_drift])
 
 
@@ -69,13 +69,14 @@ def get_eff_currency(Y, tilde_Y, tau1):
     return np.divide((Y - tilde_Y), tau1)
 
 
-def get_currency_drift(tilde_Y, alpha, tau2, E):
+def get_currency_drift(tilde_Y, alpha, tau2, g, E, d):
     """
     delt_mu = (alpha .* N - mu) ./ tau2
     """
     lambda_sum = get_lambda_sum(E)
     gamma_select = get_gamma_select(E)
-    x = get_X(lambda_sum, gamma_select, y)
+    A = np.ones((E.shape[0], E.shape[0]))
+    x = get_X(lambda_sum, gamma_select, g, d, E, tilde_Y)
     q = get_q(A, x)
     N = get_N(x, tilde_Y, lambda_sum, gamma_select, q)
     return np.divide(np.multiply(alpha, N), tau2)
@@ -90,34 +91,35 @@ def get_gamma_select(E):
 
 
 def get_N(x, tilde_Y, lambda_sum, gamma_select, q):
-    export_flow = lambda_sum @ x
-    import_flow = np.divide(lambda_sum @ np.multiply((gamma_select @ tilde_Y), q), tilde_Y)
+    export_flow = np.transpose(lambda_sum) @ x
+    import_flow = np.divide(np.transpose(lambda_sum) @ np.multiply((gamma_select @ tilde_Y), q), tilde_Y)
     return export_flow - import_flow
 
 
-def get_X(lambda_sum, gamma_select, y):
-    R = get_R()
-    return np.divide(, R)
+def get_X(lambda_sum, gamma_select, g, d, E, tilde_Y):
+    R = get_R(d, E, tilde_Y)
+    return np.divide(np.multiply(lambda_sum @ g, gamma_select @ g), R)
 
 
 def get_q(A, x):
     return A @ x
 
 
-def get_R():
-    return
+def get_R(d, E, tilde_Y):
+    exponential = np.exp(E @ tilde_Y)
+    return np.multiply(d, exponential)
 
 
 def get_E(config):
     E = []
     with open(config, 'r') as f:
         for line in f.readlines():
-            E.append(line.strip('\n').split('\t'))
+            E.append([float(x) for x in line.strip('\n').split('\t')])
     return np.array(E)
 
 
 if __name__ == '__main__':
     n = 3
-    E = get_E('test.txt')
+    E = get_E('configs/test.txt')
     x, p, u = generate_inputs(n, E)
-    evalf(x, p, u)
+    evalf(x, p, u, E)
