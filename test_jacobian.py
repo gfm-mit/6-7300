50a2bb5c62f2de42269dd8da0200fa7234823c8e
Jillian Ross <jillianr@mit.edu>
2023-09-26 11:56:15 -0400

    Change to pytest form

--- test_jacobian.py
+++ test_jacobian.py
@@ -3,12 +3,16 @@ from evalf import evalf
 from test_evalf import generate_inputs
 from jacobian import finiteDifferenceJacobian, evalJacobian
 
-if __name__ == '__main__':
+
+def test_structure():
     x, p, u = generate_inputs(3)
     print(x)
     print(p)
     print(u)
 
-    print(finiteDifferenceJacobian(evalf, x, p, u, 1e-8))
+    J_finite = finiteDifferenceJacobian(evalf, x, p, u, 1e-8)
     print()
-    print(evalJacobian(x, p, u)) 
+    J_analytical = evalJacobian(x, p, u)
+
+    assert(J_finite == J_analytical)
+
